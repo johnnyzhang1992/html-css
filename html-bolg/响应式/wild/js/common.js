e36028f02a27378a0c732750dfe8d39f710d54c9
johnnyzhang <1113638674@qq.com>
2016-10-11 18:29:10 +0800

    上传基于bootstrap的纯静态html博客网页案例

---
+++ html-bolg/响应式/wild/js/common.js
@@ -0,0 +1,24 @@
+//返回顶部
+    jQuery(document).ready(function(){
+        // hide #back-top first
+        jQuery("#topon").hide();
+        // fade in #back-top
+        jQuery(function () {
+            jQuery(window).scroll(function () {
+                if (jQuery(this).scrollTop() > 250) {
+                    jQuery('#topon').fadeIn();
+                } else {
+                    jQuery('#topon').fadeOut();
+                }
+            });
+
+            // scroll body to 0px on click
+            jQuery('#topon .to_top').click(function () {
+                jQuery('body,html').animate({
+                    scrollTop: 0
+                }, 800);
+                return false;
+            });
+        });
+
+    });
